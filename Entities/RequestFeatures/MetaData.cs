984b02eb65e77db83a039e3ee7e5092764481ea0
Gokhan Kus <gkus1998@hotmail.com>
2024-09-02 15:29:52 +0300

    Metadata.cs and PagedList of T classes

---
+++ Entities/RequestFeatures/MetaData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RequestFeatures
+{
+	public class MetaData
+	{
+		public int CurrentPage { get; set; }
+		public int TotalPage { get; set; }
+		public int PageSize { get; set; }
+		public int TotalCount { get; set; }
+		public bool HasPreviousPage => CurrentPage > 1; //bulundugumuz sayfa 1den buyukse onceki sayfa vardır HasPreviousPage=true
+		public bool HasNextPage => CurrentPage < TotalPage;//bulundugumuz sayfa TotalPage'den kucukse sonraki sayfa vardir HasNextPage = true
+	}
+}
---
+++ Entities/RequestFeatures/PagedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RequestFeatures
+{
+	public class PagedList<T> : List<T> where T : class
+	{
+		public MetaData MetaData { get; set; }
+		public PagedList(List<T> items, int count, int pageNumber, int pageSize)//List<T> items, ornegin List<Exercise> Exercises (IEnumerable<Exercise> Exercises) olarak dusunulebilir 
+		{
+			MetaData = new MetaData
+			{
+				TotalCount = count,
+				CurrentPage = pageNumber,
+				PageSize = pageSize,
+				TotalPage = (int)Math.Ceiling(count / (double)pageSize)
+			};
+			AddRange(items); //List<T>'de gelen degerler PagedList<T>'e tasinacak
+		}
+		public static PagedList<T> ToPagedList(IEnumerable<T> source, int pageNumber, int pageSize)
+		{
+			var count = source.Count();
+			var items = source
+				.Skip((pageNumber - 1) * pageSize) // IIIII IIIII IIIII örnegin 3.sayfaya gitmek istersem (3-1) * 5 = 10 tane item atlayacagimi soyluyorum
+				.Take(pageSize)
+				.ToList();
+			return new PagedList<T>(items, count, pageNumber, pageSize);
+		}
+	}
+}
