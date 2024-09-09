8829e50b84daca9c8d0ae5bb5ed8ba51f0beaadf
Gokhan Kus <gkus1998@hotmail.com>
2024-09-09 11:47:12 +0300

    DataShaper contract&concrete classes and Fields prop in RequestParameters.cs

--- Entities/RequestFeatures/RequestParameters.cs
+++ Entities/RequestFeatures/RequestParameters.cs
@@ -5,14 +5,14 @@
 		const int DefaultPageSize = 10; // Sabit değer 10 olarak ayarlanacak
 		const int maxPageSize = 50; //istemciye max 50 kaynak verilsin
 		public int PageNumber { get; set; }
-
 		//full-property
 		private int _pageSize = DefaultPageSize;
-		public int PageSize
+        public int PageSize
 		{
 			get { return _pageSize; }
 			set { _pageSize = value > maxPageSize ? maxPageSize : value; }
 			//value 50den buyukse(istemci 50'den fazla kaynak isterse max 50, 50'den kucukse istenilen deger gelsin)
 		}
+		public string? Fields { get; set; }
 	}
 }
---
+++ Services/ServiceContracts/IDataShaper.cs
@@ -0,0 +1,21 @@
+﻿using System.Dynamic;
+
+namespace Services.ServiceContracts
+{
+	public interface IDataShaper<T> where T : class
+	{
+		//ExpandoObject: memberlari runtimeda dinamik olarak eklenebilen ve kaldirilabilen bir obje saglar
+		IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString);
+		ExpandoObject ShapeData(T entity, string fieldsString);
+	}
+}
+/*
+Data Shaping: 
+API tuketicisinin, sorgu dizesi araciligiyla talep ettigi nesnenin alanlarini secerek sonuc setini sekillendirmesini saglar
+her api'nin ihtiyaci olmayabilir karmasık ve yogun trafik var ise api'mizde o zaman kullanilabilir ve boyle bir implementasyon yapilacaksa;
+runtime'da -calisma zamaninda- kod yaziyoruz ve runtime'da nesne uretmek ilgili nesnenin alanlarini secmek ve buna ait donusleri istemciye yapmak maliyetli istir
+ornek queryler;
+workouts?fields = id,totalcaloriesburned
+exercises?fields = id, name, description
+bodymeasurements?fields = id, weight
+*/
